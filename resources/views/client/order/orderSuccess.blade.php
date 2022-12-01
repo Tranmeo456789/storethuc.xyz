045d53a2b01d2d14ac78c2dfe6cd3f2aa3be6897
Tranmeo456789 <trantrantrantrantran726@gmail.com>
2022-12-02 00:01:06 +0700

    respon order success

--- resources/views/client/order/orderSuccess.blade.php
+++ resources/views/client/order/orderSuccess.blade.php
@@ -26,110 +26,117 @@
                 <div class="section-head mb-1">
                     <h5 class="section-title">Mã đơn hàng: <span class="text-success">{{$data['code_order']}}</span></h5>
                 </div>
-                <div class="section-detail">
-                    <div class="section">
-                        <div class="section-head mt-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="d-block ml-2">Thông tin khách hàng:</h5>
-                                    <!-- <table class="table table-bordered">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">Họ Tên</th>
-                                                <th scope="col">Số điện thoại</th>
-                                                <th scope="col">Địa chỉ</th>
-                                                <th scope="col">Email</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>{{$data['fullname']}}</td>
-                                                <td>{{$data['phone']}}</td>
-                                                <td>{{$data['address']}}</td>
-                                                <td>{{$data['email']}}</td>
-                                            </tr>
-                                        </tbody>
-                                    </table> -->
-                                    <div class="container-fluid">
-                                        <div class="hidden-when-768">
-                                            <div class="row">
-                                                <div class="col-md-2 wp-item-table">
-                                                    <span class="font-weight-bold">Họ tên</span>
-                                                </div>
-                                                <div class="col-md-2 wp-item-table">
-                                                    <span class="font-weight-bold">Số điện thoại</span>
-                                                </div>
-                                                <div class="col-md-5 wp-item-table text-center">
-                                                    <span class="font-weight-bold">Địa chỉ</span>
-                                                </div>
-                                                <div class="col-md-3 wp-item-table">
-                                                    <span class="font-weight-bold">Email</span>
-                                                </div>
-                                            </div>
-                                        </div>
+            </div>
+        </div>
+    </div>
+    <div class="section-detail wp-inner-fluid">
+        <div class="section">
+            <div class="section-head mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="d-block ml-2">Thông tin khách hàng:</h5>
+                        <div class="container-fluid">
+                            <div class="hidden-when-768">
+                                <div class="row">
+                                    <div class="col-md-2 wp-item-table">
+                                        <span class="font-weight-bold">Họ tên</span>
+                                    </div>
+                                    <div class="col-md-2 wp-item-table">
+                                        <span class="font-weight-bold">Số điện thoại</span>
+                                    </div>
+                                    <div class="col-md-5 wp-item-table text-center">
+                                        <span class="font-weight-bold">Địa chỉ</span>
+                                    </div>
+                                    <div class="col-md-3 wp-item-table">
+                                        <span class="font-weight-bold">Email</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-2 wp-item-table">
+                                    <span class="font-weight-bold view-when-768">Họ tên: </span><span class="">{{$data['fullname']}}</span>
+                                </div>
+                                <div class="col-md-2 wp-item-table mt-table-1">
+                                    <span class="font-weight-bold view-when-768">Số điện thoại: </span><span class="">{{$data['phone']}}</span>
+                                </div>
+                                <div class="col-md-5 wp-item-table mt-table-1">
+                                    <span class="font-weight-bold view-when-768">Địa chỉ: </span><span class="">{{$data['address']}}</span>
+                                </div>
+                                <div class="col-md-3 wp-item-table mt-table-1">
+                                    <span class="font-weight-bold view-when-768">Email: </span><span class="">{{$data['email']}}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mt-table-1">
+                    <div class="card-body">
+                        <h5 class="d-block ml-2">Thông tin đơn hàng:</h5>
+                        <div class="container-fluid">
+                            <div class="hidden-when-768">
+                                <div class="row wp-item-table">
+                                    <div class="col-1 font-weight-bold text-center"><span>STT</span></div>
+                                    <div class="col-3 col-md-2 text-center font-weight-bold">Hình ảnh</div>
+                                    <div class="col-8 col-md-9">
                                         <div class="row">
-                                            <div class="col-md-2 wp-item-table">
-                                                <span class="font-weight-bold view-when-768">Họ tên: </span><span class="">{{$data['fullname']}}</span>
+                                            <div class="col-12 col-md-6 font-weight-bold">
+                                                Tên sản phẩm
                                             </div>
-                                            <div class="col-md-2 wp-item-table mt-table-1">
-                                                <span class="font-weight-bold view-when-768">Số điện thoại: </span><span class="">{{$data['phone']}}</span>
+                                            <div class="col-6 col-md-3 font-weight-bold text-center">
+                                                Số lượng
                                             </div>
-                                            <div class="col-md-5 wp-item-table mt-table-1">
-                                                <span class="font-weight-bold view-when-768">Địa chỉ: </span><span class="">{{$data['address']}}</span>
-                                            </div>
-                                            <div class="col-md-3 wp-item-table mt-table-1">
-                                                <span class="font-weight-bold view-when-768">Email: </span><span class="">{{$data['email']}}</span>
+                                            <div class="col-6 col-md-3">
+                                                <span class="font-weight-bold">Thành tiền</span>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="d-block ml-2">Thông tin đơn hàng:</h5>
-                                    <table class="table table-bordered">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">STT</th>
-                                                <th scope="col">Ảnh</th>
-                                                <th scope="col">Tên sản phẩm</th>
-                                                <th scope="col">Số lượng</th>
-                                                <th scope="col">Thành tiền</th>
-                                            </tr>
-                                        </thead>
-                                        @php
-                                        $temp=0;
-                                        @endphp
-                                        <tbody>
-                                            @foreach ($product as $item8)
-                                            @php
-                                            $temp++;
-                                            @endphp
-                                            <tr>
-                                                <td>{{$temp}}</td>
-                                                <td style="max-width:80px;"><img src="{{asset($item8['thumbnail'])}}" alt="" class="img-fluid" style="width: 100%"></td>
-                                                <td>
-                                                    <p class="truncate3 text-primary">{{$item8['name']}}</p>
-                                                    <small><span class="font-weight-bold">Đơn giá: </span><span class="text-dart">{{number_format($item8['price_current'], 0, "" ,"." )}}đ / {{$item8['unit']}}</span></small>
-                                                </td>
-
-                                                <td>{{$item8['qty']}}</td>
-                                                <td><span class="font-weight-bold">{{number_format($item8['price_current']*$item8['qty'], 0, "" ,"." )}}đ</span></td>
-                                            </tr>
-                                            @endforeach
-                                            <tr>
-                                                <th colspan="4">Giá trị đơn hàng</th>
-                                                <td><span class="text-danger font-weight-bold">{{number_format($order['price_total'], 0, "" ,"." )}}đ</span></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                            @php
+                            $temp=0;
+                            @endphp
+                            @foreach ($product as $item8)
+                            @php
+                            $temp++;
+                            @endphp
+                            <div class="row wp-item-table ls-item-order-success mb-1px">
+                                <div class="col-1 text-center col-item-info">
+                                    <span>{{$temp}}</span>
+                                </div>
+                                <div class="col-3 col-md-2 col-item-info">
+                                    <div class="wp-img-ordersuccess">
+                                        <img src="{{asset($item8['thumbnail'])}}" alt="" class="img-fluid">
+                                    </div>
+                                </div>
+                                <div class="col-8 col-md-9 col-item-info">
+                                    <div class="row row-mr0">
+                                        <div class="col-12 col-md-6 col-item-info">
+                                            <p class="truncate3 text-primary mb-0">{{$item8['name']}}</p>
+                                            <small><span class="font-weight-bold">Đơn giá: </span><span class="text-dart">{{number_format($item8['price_current'], 0, "" ,"." )}}đ / {{$item8['unit']}}</span></small>
+                                        </div>
+                                        <div class="col-6 col-md-3 item-center-768 col-item-info">
+                                            <span class="font-weight-bold view-when-768">Số lượng: </span><span>{{$item8['qty']}}</span>
+                                        </div>
+                                        <div class="col-6 col-md-3 col-item-info monney-item">
+                                            <span class="font-weight-bold">{{number_format($item8['price_current']*$item8['qty'], 0, "" ,"." )}}đ</span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="back-home">
-                                <a href="{{url('/')}}" title="">Quay lại trang chủ</a>
+                            @endforeach
+                            <div class="row wp-item-table">
+                                <div class="col-6 font-weight-bold">
+                                    Giá trị đơn hàng
+                                </div>
+                                <div class="col-6 text-right">
+                                    <span class="text-danger font-weight-bold">{{number_format($order['price_total'], 0, "" ,"." )}}đ</span>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="back-home">
+                    <a href="{{url('/')}}" title="">Quay lại trang chủ</a>
                 </div>
             </div>
         </div>
